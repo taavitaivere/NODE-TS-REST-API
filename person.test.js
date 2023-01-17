77985a8d3dc18fd839a270e018fb051bf5531552
Taavi <taavi.taivere@khk.ee>
2023-01-17 23:10:57 +0200

    should work

--- person.test.js
+++ person.test.js
@@ -3,3 +3,9 @@ const sum = require('./src/app.ts');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+afterAll(async done => {
+  // Closing the DB connection allows Jest to exit successfully.
+  dbConnection.close();
+  done();
+});
