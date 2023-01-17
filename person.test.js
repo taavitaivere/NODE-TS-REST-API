99e7bf056f38143d444667901a462508b4a99b5f
Taavi <taavi.taivere@khk.ee>
2023-01-17 23:23:47 +0200

    should work

--- person.test.js
+++ person.test.js
@@ -1,18 +1,4 @@
 const sum = require('./src/app.ts');
-const connection = require("./src/db/config");
-const { Person } = connection.models;
-
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
-});
-
-describe(' ', () => {
-  beforeEach(async () => {
-    await Person.create({});
-  });
-});
-afterAll(async done => {
-  // Closing the DB connection allows Jest to exit successfully.
-  connection.close();
-  done();
 });
