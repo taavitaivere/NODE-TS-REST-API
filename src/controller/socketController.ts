e89ae33bbd08371ab6271b6b077dad70b478acc1
Taavi <taavi.taivere@khk.ee>
2023-05-20 00:40:48 +0300

    Adding Update person controller for socket index

--- src/controller/socketController.ts
+++ src/controller/socketController.ts
@@ -8,8 +8,13 @@ const createPerson = (io: { emit: (arg0: string, arg1: any) => any; }, data: any
     Persons.create(data).then(() => getAllPersons(io));
 }
 
+const updatePerson = (io: { emit: (arg0: string, arg1: any) => any; }, data: { id: any; }) => {
+    Persons.update(data, {where: {id: data.id}}).then(() => getAllPersons(io));
+}
+
 
 module.exports = {
     getAllPersons,
-    createPerson
+    createPerson,
+    updatePerson
 }
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,10 +1,14 @@
-const { getAllPersons, createPerson} = require('../controller/socketController');
+const { getAllPersons, createPerson, updatePerson} = require('../controller/socketController');
 
 module.exports = (io: { on: (arg0: string, arg1: (arg0: any) => void) => void; }) => {
 
     io.on('connection', (socket: { on: (arg0: string, arg1: (arg0: any) => void) => void; }) => {
+
         socket.on('get/persons', () => getAllPersons(io));
+
         socket.on('create/person', (data: any) => createPerson(io, data));
+
+        socket.on('update/person', (data: any) => updatePerson(io, data));
 
         io.on('connect_error', (err: any) => {
             console.log(`connect_error due to ${err.message}`);
