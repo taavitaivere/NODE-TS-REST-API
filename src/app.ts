56c88300f1f3b9a5d0c068fa942a3f081ed3c1a2
Taavi <taavi.taivere@khk.ee>
2023-05-20 00:23:36 +0300

    First implementation of socket

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import express from 'express';
 import connection from "./db/config";
+import router from "./routes/router";
+import cors from 'cors';
+import { Server } from 'socket.io';
+import * as http from "http";
 
 const app = express();
+export const server = http.createServer(app);
 
+export const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3001",
+    }
+});
+
+require('./socket')(io);
+
+app.use("/persons", router);
+app.use(cors());
 app.use((
     err: Error,
     req:express.Request,
