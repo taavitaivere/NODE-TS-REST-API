2bc60a118db2f67e1ab098aab98bc2b05ada6738
Taavi <taavi.taivere@khk.ee>
2023-05-19 23:22:31 +0300

    implemented get persons by id to controller and add also update person functionality

--- src/controller/personController.ts
+++ src/controller/personController.ts
@@ -1,16 +1,37 @@
 import {RequestHandler} from "express";
 import {Persons} from "../interfaces/persons";
 
+
 export const createPerson: RequestHandler = async (req, res, next) => {
     const persons = await Persons.create({...req.body});
     return res
         .status(200)
         .json({message: "Persons fetched successfully", data: persons});
 };
-
 export const getAllPerson: RequestHandler = async (req, res, next) => {
     const allPersons: Persons[] = await Persons.findAll();
 
     return res.status(200).json({message: "All persons fetched successfully", data: allPersons});
 
+}
+
+export const getPersonById: RequestHandler = async (req, res, next) => {
+    const {id} = req.params;
+    const person: Persons | null = await Persons.findByPk(id);
+
+    return res.status(200).json({message: "Person fetched successfully", data: person});
+}
+
+export const updatePerson: RequestHandler = async (req, res, next) => {
+    const {id} = req.params;
+    const person: Persons | null = await Persons.findByPk(id);
+
+    if (person) {
+        await Persons.update({...req.body}, {where: {id}});
+        const updatedPerson: Persons | null = await Persons.findByPk(id);
+
+        return res.status(200).json({message: "Person updated successfully", data: updatedPerson});
+    }
+
+    return res.status(404).json({message: "Person not found"});
 }
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,12 +2,17 @@ import {Router} from "express";
 
 import {
     createPerson,
-    getAllPerson
+    getAllPerson,
+    getPersonById,
+    updatePerson,
 } from "../controller/personController";
 
 const router = Router();
 
 router.post("/", createPerson);
 router.get("/", getAllPerson);
+router.get("/:id", getPersonById);
+router.put("/:id", updatePerson);
+
 
 export default router;
