22e9859c22285641b598889ced855f5bf08fcd8c
Taavi <taavi.taivere@khk.ee>
2023-05-21 21:32:17 +0300

    added verifyToken middleware for Create person route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -9,11 +9,33 @@ import {
 } from "../controller/personController";
 
 const router = Router();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
-router.post("/", createPerson);
+router.post("/", [verifyToken], createPerson);
 router.get("/", getAllPerson);
 router.get("/:id", getPersonById);
 router.put("/:id", updatePerson);
 router.delete("/:id", deletePerson);
 
+function verifyToken(req : any, res : any, next : any) {
+    const authHeader = req.headers['authorization'];
+
+    if (typeof authHeader !== 'undefined') {
+        const token = authHeader.split(' ')[1];
+        const privateKey = process.env.KEY;
+
+        jwt.verify(token, privateKey, {algorithm: 'HS256'}, (err : any) => {
+            if (err) {
+                res.status(401).json({message: "invalid token"});
+                throw new Error("Unauthorized");
+            }
+            return next();
+        })
+    } else {
+        res.status(401).json({message: "include token"});
+        throw new Error("include token");
+    }
+}
 export default router;
